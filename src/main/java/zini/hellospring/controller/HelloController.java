cd5ac5f9f5e7359ae78523fc5996515e6b6a9324
zinirun <heojj97@naver.com>
2021-01-01 23:47:49 +0900

    update gradle

--- src/main/java/zini/hellospring/controller/HelloController.java
+++ src/main/java/zini/hellospring/controller/HelloController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
     @GetMapping("hello") //GET METHOD
     public String hello(Model model){
         // key, value
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "spring~");
         // resources > templates > hello.html
         return "hello";
     }
