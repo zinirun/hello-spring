8a8e15fad9bfe7bfe5e4564e20220aa7f0809e08
zinirun <heojj97@naver.com>
2021-01-02 15:54:29 +0900

    Add mvc template

--- src/main/java/zini/hellospring/controller/HelloController.java
+++ src/main/java/zini/hellospring/controller/HelloController.java
@@ -3,14 +3,21 @@ package zini.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello") //GET METHOD
     public String hello(Model model){
         // key, value
-        model.addAttribute("data", "spring~");
+        model.addAttribute("data", "spring");
         // resources > templates > hello.html
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
