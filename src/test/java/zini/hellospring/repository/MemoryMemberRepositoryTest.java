84026f25eaebae10341b8be71997a865451f498c
zinirun <heojj97@naver.com>
2021-01-02 16:53:15 +0900

    Add memory + junit test

---
+++ src/test/java/zini/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,57 @@
+package zini.hellospring.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import zini.hellospring.domain.Member;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach // 테스트가 수행될때마다 호출
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        // Assertions.assertEquals(member, result); // 기대값, 비교값
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        // 단어선택 + Shift F6 -> VSCode Shift D
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
